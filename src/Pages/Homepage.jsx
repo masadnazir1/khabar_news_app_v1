b5f2fd3b815ca1b9b53e5538d6b1df38da9fb0b1
M Asad Nazir <masadnazir1@gmail.com>
2025-03-15 00:45:05 +0500

    added the tabs and trending component 30 times --asad

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -7,18 +7,34 @@ import Trending from "../components/TrendingHome/Trending";
 
 const Home = () => {
   const navigate = useNavigate();
+
+  //
+  const trendingData = Array.from({ length: 30 }, (_, index) => ({
+    id: index,
+    sourcename: "BBC",
+    time: "4h ago",
+    Headline: "Russian warship: Moskva sinks in Black Sea",
+    regionname: "PK",
+    image:
+      "https://www.thenews.com.pk/assets/uploads/updates/2025-03-14/l_1291711_054459_updates.jpg",
+    Logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/BBC_World_News_2022_%28Boxed%29.svg/2048px-BBC_World_News_2022_%28Boxed%29.svg.png",
+  }));
+  //
   return (
     <div className={styles.container}>
       <HomeHeader />
       <container className={styles.content}>
-        <Trending
-          sourcename="BBC"
-          time="4h ago"
-          Headline="Russian warship: Moskva sinks in Black Sea"
-          regionname="PK"
-          image="https://www.thenews.com.pk/assets/uploads/updates/2025-03-14/l_1291711_054459_updates.jpg"
-          Logo="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/BBC_World_News_2022_%28Boxed%29.svg/2048px-BBC_World_News_2022_%28Boxed%29.svg.png"
-        />
+        {trendingData.map((item) => (
+          <Trending
+            key={item.id}
+            sourcename={item.sourcename}
+            time={item.time}
+            Headline={item.Headline}
+            regionname={item.regionname}
+            image={item.image}
+            Logo={item.Logo}
+          />
+        ))}
       </container>
       <Tabs />
     </div>
